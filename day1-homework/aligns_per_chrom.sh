bcc0226bdac4955814446a8cae12596311acfd66
stephaniemyan <syan11@jhu.edu>
2018-08-27 20:27:08 -0400

    Add updates to the chromosome alignment command

--- day1-homework/aligns_per_chrom.sh
+++ day1-homework/aligns_per_chrom.sh
@@ -1,7 +1,11 @@
 # calculates alignments per chromosome when given a .sam file
 
+# this is an efficient-ish method with databash
 #!/bin/bash
 
 FILE=$1
 
 grep -v "^@" $FILE | grep -v 2110000 | datamash -s -g 3 count 3
+
+# a less efficient method uses sort, something like:
+# sort -k 3 $FILE | uniq -f 2 -c | head
