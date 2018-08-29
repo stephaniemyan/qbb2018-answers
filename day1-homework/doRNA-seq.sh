551a2bc55aa335624bd3157bfad97cc14789ebc5
stephaniemyan <syan11@jhu.edu>
2018-08-29 00:16:13 -0400

    Corrections to shell script

--- day1-homework/doRNA-seq.sh
+++ day1-homework/doRNA-seq.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
-GENOME=../genomes/BDGP6
-ANNOTATION=../genomes/BDGP6.Ensembl.81.gtf
-FOLDER=$1
+# doRNA-seq.sh runs FastQC, HISAT2, SAMtools, and StringTie on a and reference genome of your choice
+# You must run this in a directory that includes your .fastq read files
+# No other inputs needed in the command line
 
+# UPDATE with filepath for HISAT2 index for your reference genome
+GENOME=~/qbb2018-answers/genomes/BDGP6
+# UPDATE with filepath for your reference genome
+ANNOTATION=~/qbb2018-answers/genomes/BDGP6.Ensembl.81.gtf
+
+# UPDATE with names of your sample files
 for SAMPLE in SRR072893 SRR072903 SRR072905 SRR072915
+
 do
+	# Make new folder
     mkdir $SAMPLE
 
-    # generates quality control report for sample reads
-    fastqc $FOLDER/$SAMPLE.fastq
+    # Generate quality control report for sample reads
+	echo 'Running FastQC...'
+    fastqc -o $SAMPLE $SAMPLE.fastq
 
-    # maps sample reads to BDGP6, outputs as .sam
-    hisat2 -x $GENOME -U $FOLDER/$SAMPLE.fastq -S $SAMPLE.sam
+    # Map sample reads to reference genome and output as .sam
+	echo 'Running HISAT2...'
+    hisat2 -x $GENOME -U $SAMPLE.fastq -S $SAMPLE/$SAMPLE.sam
     
-    # converts .sam file into a sorted .bam file and .bam index
-    samtools sort $SAMPLE.sam -o ${SAMPLE}_sorted.bam
-    samtools index ${SAMPLE}_sorted.bam ${SAMPLE}_index.bam
+    # Convert .sam file into a sorted .bam file and .bam index
+	echo 'Sorting .sam file...'
+    samtools sort $SAMPLE/$SAMPLE.sam -o $SAMPLE/${SAMPLE}_sorted.bam
+	echo 'Making index from .sam file...'
+    samtools index $SAMPLE/${SAMPLE}_sorted.bam $SAMPLE/${SAMPLE}_index.bam
 
-    # quantitates sorted .bam file
-    stringtie ${SAMPLE}_sorted.bam -p 5 -e -G $ANNOTATION -o ${SAMPLE}_quantitated.gtf -B
+    # Quantitate sorted .bam file
+	echo 'Running StringTie...'
+    stringtie $SAMPLE/${SAMPLE}_sorted.bam -p 5 -e -G $ANNOTATION -o $SAMPLE/${SAMPLE}_quantitated.gtf -B
 
 done
