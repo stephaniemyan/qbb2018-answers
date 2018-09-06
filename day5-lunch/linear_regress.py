ae0e7683840a6c6b9b01bca70e57ba8f8f78c20b
stephaniemyan <syan11@jhu.edu>
2018-09-05 23:37:38 -0400

    Add usage notes to regression scripts

--- day5-lunch/linear_regress.py
+++ day5-lunch/linear_regress.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
 """
-Usage: ./log_regress.py <tab_file>
+Usage: ./linear_regress.py <tab_file>
 
 <ctab_file> Filepath to .tab file with transcript IDs,
 and their associated histone modifications (by bigWig) and FPKMs
+
+This script takes a file with gene transcripts, their FPKMs, and bigWigAverageOverBed's
+predicted histone modifications over the promoter region (for as many types of histone
+modification as you like). It fits a multiple linear regression model to the data, 
+trying to use histone modifications to predict a transcript's FPKM.
+
+It then compares the model's projected FPKM values to the actual FPKMs in the .tab
+file, and plots the residuals in histogram format.
 
 """
 
@@ -18,6 +26,7 @@ from sklearn import datasets
 
 # Read .tab file
 filename = pd.read_table(sys.argv[1])
+
 
 # Fit linear regression model to .tab data
 # Y: FPKM values, X: Corresponding avg signal for each of 5 histone markers
@@ -37,12 +46,13 @@ print(results.summary())
 # Make histogram
 
 # new_y - FPKM values predicted by the model for each transcript
-# In the histogram, we want to plot the difference between the predicted FPKM values and the actual values from the .tab file (i.e. the 'residuals')
+# In the histogram, we want to plot the difference between the predicted FPKM values
+# and the actual values from the .tab file (i.e. the 'residuals')
 new_y = results.fittedvalues
 r = new_y - Y
 
 # For the purpose of aesthetic, the xlim is set smaller than this histogram's default range
-# This cuts out one small group of residuals in the -30000s or so. ALl the other residuals lie around 0
+# This cuts out one small group of residuals in the -6000s or so. All the other residuals lie around 0
 fig, ax = plt.subplots(figsize=(8, 6))
 ax.hist(r, bins=1000)
 ax.set_xlim(-500,500)
--- day5-lunch/log_regress.py
+++ day5-lunch/log_regress.py
@@ -6,6 +6,16 @@ Usage: ./log_regress.py <tab_file>
 <ctab_file> Filepath to .tab file with transcript IDs,
 and their associated histone modifications (by bigWig) and FPKMs
 
+This script takes a file with gene transcripts, their FPKMs, and bigWigAverageOverBed's
+predicted histone modifications over the promoter region (for as many types of histone
+modification as you like). It fits a multiple linear regression model to the data, 
+trying to use histone modifications to predict a transcript's FPKM.
+
+To make the data nicer, here we use log(FPKM + 1) instead of FPKM to fit the linear 
+regression model. This results in some strange artifacts.
+
+The script then compares the model's projected FPKM values to the actual FPKMs in the 
+.tab file, and plots the residuals in histogram format.
 """
 
 import sys
@@ -19,8 +29,9 @@ from sklearn import datasets
 # Read .tab file
 filename = pd.read_table(sys.argv[1])
 
+
 # Fit linear regression model to .tab data
-# Y: Log of FPKM values
+# Y: Log of FPKM values + 1 (to account for zeroes)
 # X: Corresponding avg signal for each of 5 histone markers
 Y = np.log(1 + filename.loc[:,"FPKM"])
 X = filename.iloc[:,2:]
@@ -31,21 +42,21 @@ model = sm.OLS(Y,X)
 results = model.fit()
 results.params
 results.tvalues
+# Print summary to output R^2, p-value, coefficients, etc
 print(results.summary())
 
 
 # Make histogram
 
 # new_y - FPKM values predicted by the model for each transcript
-# In the histogram, we want to plot the difference between the predicted FPKM values and the actual values from the .tab file (i.e. the 'residuals')
+# In the histogram, we want to plot the difference between the predicted FPKM values
+# and the actual values from the .tab file (i.e. the 'residuals')
 new_y = results.fittedvalues
 r = new_y - Y
 
 # For the purpose of aesthetic, the xlim is set smaller than this histogram's default range
-# This cuts out one small group of residuals in the -6000s or so. ALl the other residuals lie around 0
 fig, ax = plt.subplots(figsize=(8, 6))
 ax.hist(r, bins=500)
-#ax.set_xlim(-500,500)
 ax.set_ylabel("Frequency")
 ax.set_xlabel("Residuals")
 ax.set_title("Linear regression residuals")
